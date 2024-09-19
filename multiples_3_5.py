66035d59a2623ebbeae4c1d6c352b7c6da19c1fc
AvantikaSharma3357 <181284090+AvantikaSharma3357@users.noreply.github.com>
2024-09-19 11:00:24 -0700

    Add Assignment 1 to Repo

---
+++ multiples_3_5.py
@@ -0,0 +1,11 @@
+'''Write a Python script to solve the following:
+If we list all the natural numbers below 10 that are multiples of 
+3 or 5, we get (3, 5, 6, 9). The sum of these multiples is 23. 
+Find the sum of all the multiples of 3 or 5 below 1000.'''
+
+sum_of_multiples = 0
+for x in range(1000):
+    if x % 3 == 0 or x % 5 == 0:
+        sum_of_multiples += x
+
+print(sum_of_multiples)
